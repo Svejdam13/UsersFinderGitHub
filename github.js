17a41d9a80e81e06d4057f380d2727e13b6598d2
Martin Svejda <svejdam13@gmail.com>
2020-06-08 19:16:55 +0100

    update

--- github.js
+++ github.js
@@ -3,13 +3,19 @@ class GitHub {
   constructor(){
     this.client_id = '5d6bc13ae7c5be398676';
     this.client_secret = '9389a8d58b4722287c55f641d2e97f18414c393a';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
   async getUser(user){
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
     
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
     return {
-      profile
+      profile,
+      repos       // same as repos:repos
     }
   }
-}
+} 
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Finder Users with GitHub accounts
+
+- Javascript App. It was built according to video course with modifications
+
+![](github.png)
